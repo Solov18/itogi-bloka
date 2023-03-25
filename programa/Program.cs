7db87ffff19c4ab74dd0650b2f3d7e458664c568
Solov18 <solov18@list.ru>
2023-03-25 15:23:50 +0300

    Создал массив и функцию рандома

--- programa/Program.cs
+++ programa/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int size = 10;
+string[] arrayOne = new string[size];
+FillRandArr(arrayOne);
+
+void FillRandArr(string[] arr)
+{
+    Random rand = new Random();
+    string AllSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*";
+    for (int i = 0; i < size; i++)
+    {
+        int randElemSize = rand.Next(1,8);
+        for (int j = 0; j < randElemSize; j++)
+        {
+            arr[i] += AllSymbols[rand.Next(0,AllSymbols.Length)];
+        }        
+    }
+}
